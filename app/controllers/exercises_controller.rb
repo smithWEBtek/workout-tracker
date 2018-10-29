29ec86aca859b709930f8b99cea25d149381bbda
github username <github email address>
2018-10-29 18:02:13 -0400

    creat CRUD controller methods, index && show html pages for exercises and workouts

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,28 @@
 class ExercisesController < ApplicationController
+  before_action :set_exercise, only: [:show]
+
+  def index
+    @exercises = Exercise.all
+    #render json: @exercises
+  end
+
+  def show
+    @exercise = Exercise.find(params[:id])
+  end
+
+  def new
+    @exercise = Exercise.create(exercise_params)
+  end
+
+
+
+  private
+
+    def exercise_params
+      params.require(:name).permit(:equipment, :description, :muscles)
+    end
+
+    def set_exercise
+      @exercise = Exercise.find(params[:id])
+    end
 end
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,30 @@
 class WorkoutsController < ApplicationController
+
+  before_action :set_workout, only: [:show]
+
+  def index
+    @workouts = Workout.all
+    #render json: @workouts
+  end
+
+  def show
+    @workout = Workout.find(params[:id])
+  end
+
+  def new
+    @workout = Workout.create(workout_params)
+  end
+
+
+
+  private
+
+    def workout_params
+      params.require(:name).permit(:description, :day)
+    end
+
+    def set_workout
+      @workout = Workout.find(params[:id])
+    end
+
 end
