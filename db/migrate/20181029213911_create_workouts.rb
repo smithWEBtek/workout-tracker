f7375520610c150c3bc7e9ac42022684312369a3
github username <github email address>
2018-10-29 17:45:41 -0400

    generate workout migrations

---
+++ db/migrate/20181029213911_create_workouts.rb
@@ -0,0 +1,11 @@
+class CreateWorkouts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :workouts do |t|
+      t.string :name
+      t.string :day
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
