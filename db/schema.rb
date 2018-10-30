95a88c9a9c9c4e15afb604898da39933ae4e9508
github username <github email address>
2018-10-29 20:38:21 -0400

    add Days migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_214615) do
+ActiveRecord::Schema.define(version: 2018_10_30_003735) do
+
+  create_table "days", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
