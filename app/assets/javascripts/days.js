fa18be4512a34b6fa1bf2ddaa6e2fd38a7f983ac
github username <github email address>
2018-10-29 20:47:45 -0400

    create days.js, define ajax method, define CRUD methods in days_controller.rb

--- app/assets/javascripts/days.js
+++ app/assets/javascripts/days.js
@@ -1,3 +1,16 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+ var url = ""
+
+$(function(){
+
+  // Pull Days from API using AJAX
+  $.get(url).success(function(json){
+    //debugger
+    $("div.all-days").html("")
+  })
+
+  e.preventDefault();
+})
--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,2 +1,30 @@
 class DaysController < ApplicationController
+
+  before_action :set_exercise, only: [:show]
+
+  def index
+    @days = Day.all
+    #render json: @days
+  end
+
+  def show
+    @day = Day.find(params[:id])
+  end
+
+  def new
+    @day = Day.create(exercise_params)
+  end
+
+
+
+  private
+
+    def exercise_params
+      params.require(:name)
+    end
+
+    def set_exercise
+      @day = Day.find(params[:id])
+    end
+
 end
