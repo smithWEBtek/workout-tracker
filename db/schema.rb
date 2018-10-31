e366a91be05ee4935a54be9909d7ca7a87bd8faa
github username <github email address>
2018-10-30 22:07:34 -0400

    add devise gem, install devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_213514) do
+ActiveRecord::Schema.define(version: 2018_10_31_015100) do
 
   create_table "days", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 2018_10_30_213514) do
 
   create_table "muscles", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
