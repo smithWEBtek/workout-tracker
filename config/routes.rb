4740f3a12ef07c7f19e15971c9046d05045c1bdf
github username <github email address>
2018-11-01 12:44:22 -0400

    nested exercises routes under workouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+
   resources :muscles
   resources :days
-  resources :exercises
-  resources :workouts
+
+  resources :workouts, only: [:index, :show, :create, :destroy, :update] do
+    resources :exercises, only: [:index, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
